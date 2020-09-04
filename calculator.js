8eefc925fd1671cc904189d321781fc897f5d90f
William Castillo <70246682+WilliamCastilloP@users.noreply.github.com>
2020-09-04 13:22:21 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,7 @@
+var age = prompt("what is your age?");
+var days = 365 * age;
+var hours = days * 24;
+var minutes = hours * 60; 
+var seconds = minutes * 60;
+alert("you have lived " + days + " days, " + hours + " hours," + minutes + " minutes and " + seconds + " seconds.");
+
